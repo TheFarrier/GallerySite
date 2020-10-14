e57f5b5d754e3ca32d45a5e38d034e9b4e59b082
The Farrier <nkfarrey@gmail.com>
2020-10-14 18:52:24 -0500

    imported gallery component

--- gallery-site/src/app/app.module.ts
+++ gallery-site/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { GalToolbarComponent } from './gal-toolbar/gal-toolbar.component';
 import { GalCardComponent } from './gal-card/gal-card.component';
 import { GalAccountPageComponent } from './gal-account-page/gal-account-page.component';
+import { GalGalleryComponent } from './gal-gallery/gal-gallery.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GalToolbarComponent,
     GalCardComponent,
-    GalAccountPageComponent
+    GalAccountPageComponent,
+    GalGalleryComponent,
   ],
   imports: [
     BrowserModule,
