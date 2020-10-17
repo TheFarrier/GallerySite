25648b478c915dbd3789f2805860608bfa9eda50
The Farrier <nkfarrey@gmail.com>
2020-10-16 19:39:09 -0500

    added account page parameter route

--- gallery-site/src/app/app-routing.module.ts
+++ gallery-site/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GalAccountPageComponent } from './gal-account-page/gal-account-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ':account', component: GalAccountPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
