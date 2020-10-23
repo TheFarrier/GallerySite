4ab049b820f43653b1e397d88019a3143e982659
The Farrier <nkfarrey@gmail.com>
2020-10-23 03:57:02 -0500

    removed gal-card, now part of gal-gallery

--- gallery-site/src/app/app.module.ts
+++ gallery-site/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalToolbarComponent } from './gal-toolbar/gal-toolbar.component';
-import { GalCardComponent } from './gal-card/gal-card.component';
+
 import { GalAccountPageComponent } from './gal-account-page/gal-account-page.component';
 import { GalGalleryComponent } from './gal-gallery/gal-gallery.component';
 import { LightboxModule } from 'ngx-lightbox';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GalToolbarComponent,
-    GalCardComponent,
     GalAccountPageComponent,
     GalGalleryComponent,
   ],
